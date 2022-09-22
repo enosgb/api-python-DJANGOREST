fd851e541c5839c0c1c19c8831b746ae89114499
Enos Gabriel <enosgb55@gmail.com>
2022-09-22 13:06:56 -0300

    first commit

---
+++ setup/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
+from escola.views import AlunosViewSet,CursosViewSet,MatriculasViewSet,ListaMatriculasAluno,ListaAlunosMatriculados
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('Alunos',AlunosViewSet,basename='Alunos')
+router.register('Cursos',CursosViewSet,basename='Cursos')
+router.register('Matriculas',MatriculasViewSet,basename='Matriculas')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('aluno/<int:pk>/matriculas/',ListaMatriculasAluno.as_view()),
+    path('curso/<int:pk>/matriculas/',ListaAlunosMatriculados.as_view())
+]
